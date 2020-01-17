6afb451084ffb8eb6d4cce4a6906e7c2dc7eb35c
Olga Savic <44786753+olgaSavic@users.noreply.github.com>
2020-01-17 20:22:27 +0100

    Add files via upload

---
+++ demo/src/main/java/root/demo/repository/KorisnikRepository.java
@@ -0,0 +1,13 @@
+package root.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import root.demo.model.Korisnik;
+
+public interface KorisnikRepository extends JpaRepository<Korisnik, Long> 
+{
+	public Korisnik save(Korisnik k);
+	public Korisnik findOneByEmail(String email);
+	public Korisnik findOneByUsername(String username);
+	
+}
